beb32d794ae2eca54b6fe01170fb49b7537a8e56
abudhosamuel <abudhosamuel@gmail.com>
2024-08-12 19:45:24 +0300

    crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => onDelete(id));
+  }
+
+  function handleCorrectAnswerChange(event) {
+    const updatedCorrectIndex = parseInt(event.target.value, 10);
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: updatedCorrectIndex }),
+    })
+      .then((response) => response.json())
+      .then((updatedQuestion) => onUpdate(updatedQuestion));
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +35,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
